8a85d5d189f3405db837d237dc2c246ede780181
KimJoongKi <thoug12@naver.com>
2020-12-30 15:02:02 +0900

    AppConfig 리팩터링

--- src/main/java/yaddoong/core/AppConfig.java
+++ src/main/java/yaddoong/core/AppConfig.java
@@ -1,6 +1,8 @@
 package yaddoong.core;
 
+import yaddoong.core.discount.DiscountPolicy;
 import yaddoong.core.discount.FixDiscountPolicy;
+import yaddoong.core.member.MemberRepository;
 import yaddoong.core.member.MemberService;
 import yaddoong.core.member.MemberServiceImpl;
 import yaddoong.core.member.MemoryMemberRepository;
@@ -10,13 +12,21 @@ import yaddoong.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 }
