44a0dbf4d249a4e252c01a3fc74546d65dbccd49
KimJoongKi <thoug12@naver.com>
2021-01-02 14:24:06 +0900

    싱글톤 방식의 주의점

---
+++ src/test/java/yaddoong/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,36 @@
+package yaddoong.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StatefulServiceTest {
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefullService statefullService1 = ac.getBean(StatefullService.class);
+        StatefullService statefullService2 = ac.getBean(StatefullService.class);
+
+        //ThreadA: A 사용자 10000원 주문
+        int userA = statefullService1.order("userA", 10000);
+        //ThreadB: B 사용자 20000원 주문
+        int userB = statefullService2.order("userB", 20000);
+
+        //ThreadA: 사용자A 주문 금액을 조회
+//        int price = statefullService1.getPrice();
+        System.out.println("price = " + userA);
+
+        assertThat(userA).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefullService statefullService() {
+            return new StatefullService();
+        }
+    }
+}
