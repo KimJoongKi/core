e69431389766bfef681a6bf53848888c6802611a
KimJoongKi <thoug12@naver.com>
2021-01-02 11:36:13 +0900

    웹 애플리케이션과 싱글톤

---
+++ src/test/java/yaddoong/core/singleton/SingletonTest.java
@@ -0,0 +1,27 @@
+package yaddoong.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import yaddoong.core.AppConfig;
+import yaddoong.core.member.MemberService;
+
+public class SingletonTest {
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        // 1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService = appConfig.memberService();
+
+        // 2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        // 참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService);
+        System.out.println("memberService2 = " + memberService2);
+
+        // memberSerivce1 != memberService2
+        Assertions.assertThat(memberService).isNotSameAs(memberService2);
+    }
+}
