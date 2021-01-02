d0c9aa505bbc090391338516a9c15677e5a355ea
KimJoongKi <thoug12@naver.com>
2021-01-02 11:56:44 +0900

    싱글톤 패턴

--- src/test/java/yaddoong/core/singleton/SingletonTest.java
+++ src/test/java/yaddoong/core/singleton/SingletonTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import yaddoong.core.AppConfig;
 import yaddoong.core.member.MemberService;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class SingletonTest {
     @Test
     @DisplayName("스프링 없는 순수한 DI 컨테이너")
@@ -22,6 +24,22 @@ public class SingletonTest {
         System.out.println("memberService2 = " + memberService2);
 
         // memberSerivce1 != memberService2
-        Assertions.assertThat(memberService).isNotSameAs(memberService2);
+        assertThat(memberService).isNotSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest() {
+
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        assertThat(singletonService1).isSameAs(singletonService2);
+        // same ==
+        // equal equals
+
     }
 }
