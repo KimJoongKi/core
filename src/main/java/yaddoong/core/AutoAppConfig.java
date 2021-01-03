29182a4fa12446622634491da4231facb777b66d
KimJoongKi <thoug12@naver.com>
2021-01-03 13:18:04 +0900

    중복 등록과 충돌

--- src/main/java/yaddoong/core/AutoAppConfig.java
+++ src/main/java/yaddoong/core/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package yaddoong.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import yaddoong.core.member.MemberRepository;
+import yaddoong.core.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
@@ -10,6 +13,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
